0a23a0683abd7244cf20e892f58002f2bc919971
Kousha Talebian <kousha.talebian@gmail.com>
2020-06-25 17:11:52 -0700

    remove js file

--- packages/flex-plugins-utils-exception/src/index.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-/* eslint-disable import/no-unused-modules */
-exports.__esModule = true;
-var TwilioError_1 = require("./lib/TwilioError");
-exports.TwilioError = TwilioError_1["default"];
-var TwilioApiError_1 = require("./lib/TwilioApiError");
-exports.TwilioApiError = TwilioApiError_1["default"];
